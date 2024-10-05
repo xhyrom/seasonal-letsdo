4f2642c3cc8120e9d80fcf47aba091af83baae83
Jozef Steinhübl <xhyrom.hyro@gmail.com>
2024-10-05 20:33:14 +0200

    feat: 1.21 support

---
+++ neoforge/src/main/java/dev/xhyrom/neoforge/SeasonalLetsDoModNeoForge.java
@@ -0,0 +1,11 @@
+package dev.xhyrom.neoforge;
+
+import dev.xhyrom.SeasonalLetsDoMod;
+import net.neoforged.fml.common.Mod;
+
+@Mod(SeasonalLetsDoMod.MOD_ID)
+public final class SeasonalLetsDoModNeoForge {
+    public SeasonalLetsDoModNeoForge() {
+        SeasonalLetsDoMod.init();
+    }
+}
